11d62b092fed4fed9236a2679cfff9e3942b69eb
pauldubois98 <pauldubois98@gmail.com>
2023-11-22 11:05:42 +0100

    built website (7 enigmas)

---
+++ finish_script.js
@@ -0,0 +1,24 @@
+function final_code() {
+  var password =
+    document.getElementById("password0").value + 
+    document.getElementById("password1").value + 
+    document.getElementById("password2").value + 
+    document.getElementById("password3").value + 
+    document.getElementById("password4").value + 
+    document.getElementById("password5").value + 
+    document.getElementById("password6").value;
+
+  var sha_password = SHA256(
+    password.replace(/ /g, "")
+  );
+  var user_code = sha_password.substring(0, 8).toUpperCase();
+  if (
+    SHA256(user_code) ==
+    "25d317286bc2ce57cc2f397a1457f3b6d7f2c741f6520824e575f9aa5cb627d8"
+  ) {
+    document.getElementById("finish-code").innerHTML =
+      'Finish code: <span id="code">' + user_code + "</span>";
+  } else {
+    document.getElementById("finish-code").innerHTML = "";
+  }
+}
---
+++ script.js
@@ -0,0 +1,140 @@
+
+function checkPassword0() {
+  var password = document.getElementById("password0").value;
+  var sha_password = SHA256(password.replace(/ /g, ''));
+  if (
+    sha_password ==
+    "35135aaa6cc23891b40cb3f378c53a17a1127210ce60e125ccf03efcfdaec458"
+  ) {
+    document.getElementById("enigma0-check").style = "display: block";
+    document.getElementById("enigma0-no-check").style = "display: none";
+  } else {
+    document.getElementById("enigma0-no-check").style = "display: block";
+    document.getElementById("enigma0-check").style = "display: none";
+    document.getElementById("password0").value = "";
+  }
+  final_code();
+}
+
+checkPassword0();
+
+
+function checkPassword1() {
+  var password = document.getElementById("password1").value;
+  var sha_password = SHA256(password.replace(/ /g, ''));
+  if (
+    sha_password ==
+    "d2cbad71ff333de67d07ec676e352ab7f38248eb69c942950157220607c55e84"
+  ) {
+    document.getElementById("enigma1-check").style = "display: block";
+    document.getElementById("enigma1-no-check").style = "display: none";
+  } else {
+    document.getElementById("enigma1-no-check").style = "display: block";
+    document.getElementById("enigma1-check").style = "display: none";
+    document.getElementById("password1").value = "";
+  }
+  final_code();
+}
+
+checkPassword1();
+
+
+function checkPassword2() {
+  var password = document.getElementById("password2").value;
+  var sha_password = SHA256(password.replace(/ /g, ''));
+  if (
+    sha_password ==
+    "71ee45a3c0db9a9865f7313dd3372cf60dca6479d46261f3542eb9346e4a04d6"
+  ) {
+    document.getElementById("enigma2-check").style = "display: block";
+    document.getElementById("enigma2-no-check").style = "display: none";
+  } else {
+    document.getElementById("enigma2-no-check").style = "display: block";
+    document.getElementById("enigma2-check").style = "display: none";
+    document.getElementById("password2").value = "";
+  }
+  final_code();
+}
+
+checkPassword2();
+
+
+function checkPassword3() {
+  var password = document.getElementById("password3").value;
+  var sha_password = SHA256(password.replace(/ /g, ''));
+  if (
+    sha_password ==
+    "d44adfd7777aea2a20eff9eb1177255586f385606c72659f9e93a914c8d1314d"
+  ) {
+    document.getElementById("enigma3-check").style = "display: block";
+    document.getElementById("enigma3-no-check").style = "display: none";
+  } else {
+    document.getElementById("enigma3-no-check").style = "display: block";
+    document.getElementById("enigma3-check").style = "display: none";
+    document.getElementById("password3").value = "";
+  }
+  final_code();
+}
+
+checkPassword3();
+
+
+function checkPassword4() {
+  var password = document.getElementById("password4").value;
+  var sha_password = SHA256(password.replace(/ /g, ''));
+  if (
+    sha_password ==
+    "349c41201b62db851192665c504b350ff98c6b45fb62a8a2161f78b6534d8de9"
+  ) {
+    document.getElementById("enigma4-check").style = "display: block";
+    document.getElementById("enigma4-no-check").style = "display: none";
+  } else {
+    document.getElementById("enigma4-no-check").style = "display: block";
+    document.getElementById("enigma4-check").style = "display: none";
+    document.getElementById("password4").value = "";
+  }
+  final_code();
+}
+
+checkPassword4();
+
+
+function checkPassword5() {
+  var password = document.getElementById("password5").value;
+  var sha_password = SHA256(password.replace(/ /g, ''));
+  if (
+    sha_password ==
+    "9165944995a150430ff5ee6ee2316d62e8bdfe29293e5695124f6c2ba33bf0d2"
+  ) {
+    document.getElementById("enigma5-check").style = "display: block";
+    document.getElementById("enigma5-no-check").style = "display: none";
+  } else {
+    document.getElementById("enigma5-no-check").style = "display: block";
+    document.getElementById("enigma5-check").style = "display: none";
+    document.getElementById("password5").value = "";
+  }
+  final_code();
+}
+
+checkPassword5();
+
+
+function checkPassword6() {
+  var password = document.getElementById("password6").value;
+  var sha_password = SHA256(password.replace(/ /g, ''));
+  if (
+    sha_password ==
+    "c18d4cbad6a778b727bac4c7572a747cf105ad959e684f3d3c123d3e4071a5ca"
+  ) {
+    document.getElementById("enigma6-check").style = "display: block";
+    document.getElementById("enigma6-no-check").style = "display: none";
+  } else {
+    document.getElementById("enigma6-no-check").style = "display: block";
+    document.getElementById("enigma6-check").style = "display: none";
+    document.getElementById("password6").value = "";
+  }
+  final_code();
+}
+
+checkPassword6();
+
